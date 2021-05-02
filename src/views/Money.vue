e616c977001d0c0da87944a788118697f3ccb00a
ling <l13789060033@163.com>
2021-05-02 10:20:00 +0800

    money types css

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -55,6 +55,30 @@ export default {
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
+.types{
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  >li{
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content:center ;
+    align-items: center;
+    position: relative;
+    &.selected::after{
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 3px;
+      background: #333;
+    }
+
+  }
+}
 .notes{
   font-size: 14px;
   background: #f5f5f5;

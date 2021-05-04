456b59ed80330ac9d00cd5b648a4bef56d01e7d6
ling <l13789060033@163.com>
2021-05-04 15:34:17 +0800

    id生成

---
+++ src/lib/createId.ts
@@ -0,0 +1,9 @@
+
+let id:number = parseInt(window.localStorage.getItem('_index')|| '0') || 0
+
+function createId(){
+  id++
+  return id
+}
+
+export default createId
--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from "../lib/createId";
+
 const localStorageKeyName = 'tagList'
 type Tag = {
   id: string
@@ -18,11 +20,13 @@ const tagListModel: TagListModel = {
     return this.data
   },
   create(name){
+    const id = createId().toString()
     const names = this.data.map(item => item.name)
+
     if (names.indexOf(name)>=0){
       return 'duplicated'
     }
-    this.data.push({id:name, name:name})
+    this.data.push({id, name:name})
     this.save()
     return 'success'
   },

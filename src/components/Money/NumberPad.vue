f89f61c61f3af4d424c804e90f2d62adeb7ee94a
ling <l13789060033@163.com>
2022-02-09 18:23:50 +0800

    格式化

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -26,8 +26,8 @@ import { Component, Prop } from 'vue-property-decorator';
 
 @Component
 export default class NumberPad extends Vue {
-  @Prop(Number) readonly value!:number
-  output =this.value.toString()
+  @Prop(Number) readonly value!: number;
+  output = this.value.toString();
 
   inputContent(event: MouseEvent) {
     const button = event.target as HTMLButtonElement;
@@ -43,24 +43,26 @@ export default class NumberPad extends Vue {
       }
       return;
     }
-    if (this.output.indexOf('.') >= 0 && input==='.') {return;}
-      this.output += input;
+    if (this.output.indexOf('.') >= 0 && input === '.') {
+      return;
     }
-    remove(){
-      if(this.output.length === 1){
-      this.output = '0'
-      }else{
-      this.output = this.output.slice(0,-1)
+    this.output += input;
+  }
+  remove() {
+    if (this.output.length === 1) {
+      this.output = '0';
+    } else {
+      this.output = this.output.slice(0, -1);
     }
   }
-  clear(){
-    this.output = '0'
+  clear() {
+    this.output = '0';
   }
-  ok(){
-    const number = parseFloat(this.output)
-    this.$emit('update:value', number)
-    this.$emit('submit', number)
-    this.output = '0'
+  ok() {
+    const number = parseFloat(this.output);
+    this.$emit('update:value', number);
+    this.$emit('submit', number);
+    this.output = '0';
   }
 }
 </script>

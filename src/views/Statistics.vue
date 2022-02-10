c5f75f1125472990a44ca7af844d12092f331d00
ling <l13789060033@163.com>
2022-02-10 14:40:17 +0800

    30天数据

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,200 +1,202 @@
 <template>
   <Layout>
-    <Tabs
-      class-prefix="type"
-      :data-source="recordTypeList"
-      :value.sync="type"
-    />
+    <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <div class="chart-wrapper" ref="chartWrapper">
-      <Chart :options="x" class="chart"></Chart>
+      <Chart class="chart" :options="x"/>
     </div>
-    <ol v-if="groupedList.length > 0">
+    <ol v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
-        <h3 class="title">
-          {{ beautify(group.title) }} <span>￥{{ group.total }}</span>
-        </h3>
+        <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
         <ol>
-          <li v-for="item in group.items" :key="item.id" class="record">
-            <span>{{ tagString(item.tags) }}</span>
-            <span class="notes">{{ item.notes }}</span>
-            <span>￥{{ item.amount }} </span>
+          <li v-for="item in group.items" :key="item.id"
+            class="record"
+          >
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}} </span>
           </li>
         </ol>
       </li>
     </ol>
-    <div v-else class="noResult">目前没有相关的记录</div>
+    <div v-else class="noResult">
+      目前没有相关记录
+    </div>
   </Layout>
 </template>
 <script lang="ts">
-import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
-import Tabs from '@/components/Tabs.vue';
-import recordTypeList from '@/constants/recordTypeList';
-import dayjs from 'dayjs';
-import clone from '@/lib/clone';
-import Chart from '@/components/Chart.vue'
-
-
-
-@Component({
-  components: { Tabs, Chart },
-})
-export default class Statistics extends Vue {
-  tagString(tags: Tag[]) {
-    return tags.length === 0 ? '无' : tags.map((t) => t.name).join(',');
-  }
-  mounted() {
-     const div = (this.$refs.chartWrapper as HTMLDivElement);
-      console.log(div.scrollWidth);
-      div.scrollLeft = div.scrollWidth;
-  }
-  beautify(string: string) {
-    const day = dayjs(string);
-    const now = dayjs();
-    if (day.isSame(now, 'day')) {
-      return '今天';
-    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
-      console.log('hi');
-      return '昨天';
-    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
-      return '前天';
-    } else if (day.isSame(now, 'year')) {
-      return day.format('M月D日');
-    } else {
-      return day.format('YYYY年M月D日');
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  import Tabs from '@/components/Tabs.vue';
+  import recordTypeList from '@/constants/recordTypeList';
+  import dayjs from 'dayjs';
+  import clone from '@/lib/clone';
+  import Chart from '@/components/Chart.vue';
+  import _ from 'lodash';
+  import day from 'dayjs';
+  @Component({
+    components: {Tabs, Chart},
+  })
+  export default class Statistics extends Vue {
+    tagString(tags: Tag[]) {
+      return tags.length === 0 ? '无' :
+        tags.map(t => t.name).join('，');
     }
-  }
-     get x() {
+    mounted() {
+      const div = (this.$refs.chartWrapper as HTMLDivElement);
+      div.scrollLeft = div.scrollWidth;
+    }
+    beautify(string: string) {
+      const day = dayjs(string);
+      const now = dayjs();
+      if (day.isSame(now, 'day')) {
+        return '今天';
+      } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+        console.log('hi');
+        return '昨天';
+      } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+        return '前天';
+      } else if (day.isSame(now, 'year')) {
+        return day.format('M月D日');
+      } else {
+        return day.format('YYYY年M月D日');
+      }
+    }
+    get y() {
+      const today = new Date();
+      const array = [];
+      for (let i = 0; i <= 29; i++) {
+        // this.recordList = [{date:7.3, value:100}, {date:7.2, value:200}]
+        const dateString = day(today)
+          .subtract(i, 'day').format('YYYY-MM-DD');
+        const found = _.find(this.recordList, {
+          createdAt: dateString
+        });
+        array.push({
+          date: dateString, value: found ? found.amount : 0
+        });
+      }
+      array.sort((a, b) => {
+        if (a.date > b.date) {
+          return 1;
+        } else if (a.date === b.date) {
+          return 0;
+        } else {
+          return -1;
+        }
+      });
+      return array;
+    }
+    get x() {
+      const keys = this.y.map(item => item.date);
+      const values = this.y.map(item => item.value);
       return {
-        grid:{
+        grid: {
           left: 0,
-          right:0
+          right: 0,
         },
         xAxis: {
           type: 'category',
-          data: [
-            '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-            '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-            '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-          ],
-          axisTick:{alignWithLabel: true},
+          data: keys,
+          axisTick: {alignWithLabel: true},
           axisLine: {lineStyle: {color: '#666'}}
         },
         yAxis: {
           type: 'value',
-          show:false
+          show: false
         },
         series: [{
-           symbol: 'circle',
+          symbol: 'circle',
           symbolSize: 12,
           itemStyle: {borderWidth: 1, color: '#666', borderColor: '#666'},
-          data: [
-            820, 932, 901, 934, 1290, 1330, 1320,
-            820, 932, 901, 934, 1290, 1330, 1320,
-            820, 932, 901, 934, 1290, 1330, 1320,
-            820, 932, 901, 934, 1290, 1330, 1320, 
-          ],
+          // lineStyle: {width: 10},
+          data: values,
           type: 'line'
         }],
-         tooltip: {
+        tooltip: {
           show: true, triggerOn: 'click',
           position: 'top',
           formatter: '{c}'
         }
       };
     }
-
-  get recordList() {
-    return (this.$store.state as RootState).recordList;
-  }
-  get groupedList() {
-    const { recordList } = this;
-    if (recordList.length === 0) {
-      return [];
+    get recordList() {
+      return (this.$store.state as RootState).recordList;
     }
-    const newList = clone(recordList)
-      .filter((r) => r.type === this.type)
-      .sort(
-        (a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf()
-      );
-    if (newList.length === 0) {
-      return [];
-    }
-    type Result = { title: string; total?: number; items: RecordItem[] }[];
-    const result: Result = [
-      {
-        title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'),
-        items: [newList[0]],
-      },
-    ];
-    for (let i = 1; i < newList.length; i++) {
-      const current = newList[i];
-      const last = result[result.length - 1];
-      if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
-        last.items.push(current);
-      } else {
-        result.push({
-          title: dayjs(current.createdAt).format('YYYY-MM-DD'),
-          items: [current],
-        });
+    get groupedList() {
+      const {recordList} = this;
+      const newList = clone(recordList)
+        .filter(r => r.type === this.type)
+        .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+      if (newList.length === 0) {return [];}
+      type Result = { title: string, total?: number, items: RecordItem[] }[]
+      const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+      for (let i = 1; i < newList.length; i++) {
+        const current = newList[i];
+        const last = result[result.length - 1];
+        if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+          last.items.push(current);
+        } else {
+          result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+        }
       }
+      result.map(group => {
+        group.total = group.items.reduce((sum, item) => {
+          return sum + item.amount;
+        }, 0);
+      });
+      return result;
     }
-    result.map((group) => {
-      group.total = group.items.reduce((sum, item) => {
-        console.log(sum);
-        console.log(typeof item);
-        return sum + item.amount;
-      }, 0);
-    });
-    return result;
+    beforeCreate() {
+      this.$store.commit('fetchRecords');
+    }
+    type = '-';
+    recordTypeList = recordTypeList;
   }
-  beforeCreate() {
-    this.$store.commit('fetchRecords');
-  }
-  type = '-';
-  recordTypeList = recordTypeList;
-}
 </script>
 
 <style scoped lang="scss">
-.noResult {
-  padding: 16px;
-  text-align: center;
-}
-::v-deep {
-  .type-tabs-item {
-    background: #c4c4c4;
-    &.selected {
-      background: white;
-      &::after {
-        display: none;
+  .echarts {
+    max-width: 100%;
+    height: 400px;
+  }
+  .noResult {
+    padding: 16px;
+    text-align: center;
+  }
+  ::v-deep {
+    .type-tabs-item {
+      background: #C4C4C4;
+      &.selected {
+        background: white;
+        &::after {
+          display: none;
+        }
       }
     }
+    .interval-tabs-item {
+      height: 48px;
+    }
   }
-  .interval-tabs-item {
-    height: 48px;
+  %item {
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
   }
-}
-%item {
-  padding: 8px 16px;
-  line-height: 24px;
-  display: flex;
-  justify-content: space-between;
-  align-content: center;
-}
-.title {
-  @extend %item;
-}
-.record {
-  background: white;
-  @extend %item;
-}
-.notes {
-  margin-right: auto;
-  margin-left: 16px;
-  color: #999;
-}
-.chart {
+  .title {
+    @extend %item;
+  }
+  .record {
+    background: white;
+    @extend %item;
+  }
+  .notes {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
+  }
+  .chart {
     width: 430%;
     &-wrapper {
       overflow: auto;
@@ -202,5 +204,5 @@ export default class Statistics extends Vue {
         display: none;
       }
     }
-}
+  }
 </style>
